766b1be65b729c6d18ec58044836b2630ff404f4
kyadmin <wyyservice@gmail.com>
2015-03-23 23:03:28 +0800

    This is 2015-03-23 23:03 modfiy !

--- plugins/rrdtool_drafting.py
+++ plugins/rrdtool_drafting.py
@@ -26,7 +26,7 @@ class graph_rrdtool:
     3.更新rrdtool数据库
     4.进行rrdtool绘图。
     """
-    def __init__(self,filename="/etc/otomat/otomat.cnf",graph=None,title="北京壹號車 系統報告"):
+    def __init__(self,filename="/etc/otomat/otomat.cnf",start_time="-1d"):
         self.filename = filename
 	config = conf.otomat_conf(self.filename)
 	self.host = config.rrdtool_host()
@@ -38,8 +38,8 @@ class graph_rrdtool:
 	self.graph_network = config.graph_network()
         self.graph_login = config.graph_login() 
 	self.graph_dir = config.graph_dir()
-        self.title = title
-        self.time = str(int(time.time()))
+        #self.time = str(int(time.time()))
+        self.time = start_time
 	self.graph_list = [self.graph_cpu,self.graph_mem,self.graph_disk,self.graph_network,self.graph_login]
 	
     def graph_rrdtool(self):
@@ -126,7 +126,8 @@ class graph_rrdtool:
 			'DEF:iowait=cpu.rrd:cpu_iowait:AVERAGE',
 			'DEF:steal=cpu.rrd:cpu_steal:AVERAGE',
 			'DEF:idel=cpu.rrd:cpu_idel:AVERAGE',
-			'COMMENT:\\n',
+			'COMMENT:\\r',
+			'COMMENT:\\r',
 			'LINE3:a#FFC125:Loadavg 1',
 			'GPRINT:a:LAST:Currnet\:%8.2lf %s',
 			'GPRINT:a:AVERAGE:Average\:%8.2lf %s',
@@ -178,7 +179,159 @@ class graph_rrdtool:
 				logging.warning("CPU png already exists.To be convered.")
 				os.remove(dst_file)
 				shutil.move(self.graph_cpu,dst)
+		
+		
+		### mem.png
+		title = "Mem Utilzation   ("+date+")"
+         	rrdtool.graphv(self.graph_mem,'--start',"-1d",
+			'--vertical-label=Bytes',
+			'--x-grid','MINUTE:12:HOUR:1:HOUR:1:0:%H',
+			'--width','650','--height','230',
+			'--title',title,
+			'DEF:memtotal=mem.rrd:mem_total:AVERAGE',
+			'DEF:memfreed=mem.rrd:mem_freed:AVERAGE',
+			'DEF:memused=mem.rrd:mem_used:AVERAGE',
+			'DEF:buffersfreed=mem.rrd:mem_buffers_freed:AVERAGE',
+			'DEF:buffersused=mem.rrd:mem_buffers_used:AVERAGE',
+			'DEF:mempercent=mem.rrd:mem_used_percent:AVERAGE',
+			'DEF:swaptotal=mem.rrd:swap_total:AVERAGE',
+			'DEF:swapfreed=mem.rrd:swap_freed:AVERAGE',
+			'DEF:swapused=mem.rrd:swap_used:AVERAGE',
+			'DEF:swappercent=mem.rrd:swap_used_percnet:AVERAGE',
+			'COMMENT:\\r',
+			'COMMENT:\\r',
+			'LINE3:memtotal#FFC125:Mem Total',
+			'GPRINT:memtotal:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:memtotal:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:memtotal:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE3:memused#CD2626:Mem Used',
+			'GPRINT:memused:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:memused:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:memused:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE2:memfreed#00FF00:Mem Freed',
+			'GPRINT:memfreed:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:memfreed:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:memfreed:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE3:buffersfreed#0000FF:Mem Buffers Freed',
+			'GPRINT:buffersfreed:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:buffersfreed:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:buffersfreed:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE3:buffersused#EEEE00:Mem Buffers Used',
+			'GPRINT:buffersused:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:buffersused:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:buffersused:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE3:mempercent#EE9A49:Mem Used Percent',
+			'GPRINT:mempercent:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:mempercent:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:mempercent:MAX:Maximum\:%8.2lf %s\\n', 
+			'LINE3:swaptotal#B4EEB4:SWAP Total',
+			'GPRINT:swaptotal:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:swaptotal:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:swaptotal:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE3:swapfreed#B4EEB4:SWAP Freed',
+			'GPRINT:swapfreed:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:swapfreed:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:swapfreed:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE3:swapused#B4EEB4:SWAP Used',
+			'GPRINT:swapused:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:swapused:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:swapused:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE3:swappercent#AAAAAA:SWAP Used Percent',
+			'GPRINT:swappercent:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:swappercent:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:swappercent:MAX:Maximum\:%8.2lf %s\\n')
 
-if __name__ == "__main__":
-	t = graph_rrdtool('/etc/otomat/otomat.cnf')
-	t.graph_rrdtool()
+
+		if not os.path.exists(self.graph_mem):
+			logging.error('MEM png not found. PNG file generated failure.')
+		else:
+			k = '/'
+			dst = self.graph_dir+k+j+k+today_dir
+			dst_file = dst+k+self.graph_mem
+			print dst
+			print dst_file
+			if not os.path.exists(dst_file):
+				shutil.move(self.graph_mem,dst)
+			else:
+				logging.warning("MEM png already exists.To be convered.")
+				os.remove(dst_file)
+				shutil.move(self.graph_mem,dst)
+
+		### disk.png
+		title = "Disk Utilzation   ("+date+")"
+         	rrdtool.graphv(self.graph_disk,'--start',"-1d",
+			'--vertical-label=Bytes',
+			'--x-grid','MINUTE:12:HOUR:1:HOUR:1:0:%H',
+			'--width','650','--height','230',
+			'--title',title,
+			'DEF:percent=disk.rrd:disk_used_percnet:AVERAGE',
+			'DEF:used=disk.rrd:disk_used:AVERAGE',
+			'DEF:freed=disk.rrd:disk_freed:AVERAGE',
+			'DEF:total=disk.rrd:disk_total:AVERAGE',
+			'COMMENT:\\r',
+			'COMMENT:\\r',
+			'AREA:percent#FFC125:Percent',
+			'GPRINT:percent:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:percent:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:percent:MAX:Maximum\:%8.2lf %s\\n',
+			'AREA:used#CD2626:Used',
+			'GPRINT:used:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:used:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:used:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE2:freed#00FF00:Freed',
+			'GPRINT:freed:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:freed:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:freed:MAX:Maximum\:%8.2lf %s\\n',
+			'LINE3:total#0000FF:Total',
+			'GPRINT:total:LAST:Currnet\:%8.2lf %s',
+			'GPRINT:total:AVERAGE:Average\:%8.2lf %s',
+			'GPRINT:total:MAX:Maximum\:%8.2lf %s\\n')
+
+		if not os.path.exists(self.graph_disk):
+			logging.error('DISK png not found. PNG file generated failure.')
+		else:
+			k = '/'
+			dst = self.graph_dir+k+j+k+today_dir
+			dst_file = dst+k+self.graph_disk
+			print dst
+			print dst_file
+			if not os.path.exists(dst_file):
+				shutil.move(self.graph_disk,dst)
+			else:
+				logging.warning("DISK png already exists.To be convered.")
+				os.remove(dst_file)
+				shutil.move(self.graph_disk,dst)
+
+		### login_user.png
+		title = "Lonin User Num   ("+date+")"
+         	rrdtool.graphv(self.graph_login,'--start',"-1d",
+			'--vertical-label=Bytes',
+			'--x-grid','MINUTE:12:HOUR:1:HOUR:1:0:%H',
+			'--width','650','--height','230',
+			'--title',title,
+			'DEF:o=login_user.rrd:login_user_num:AVERAGE',
+			'COMMENT:\\r',
+			'COMMENT:\\r',
+			'AREA:o#00FF00:Users',
+			'COMMENT: ',
+			'GPRINT:o:LAST:Currnet\:%8.0lf',
+			'COMMENT: ',
+			'GPRINT:o:AVERAGE:Avg login user num\:%8.0lf',
+			'COMMENT: ',
+			'GPRINT:o:MAX:Maximum\:%8.0lf\\n')
+
+
+		if not os.path.exists(self.graph_login):
+			logging.error('login png not found. PNG file generated failure.')
+		else:
+			k = '/'
+			dst = self.graph_dir+k+j+k+today_dir
+			dst_file = dst+k+self.graph_login
+			print dst
+			print dst_file
+			if not os.path.exists(dst_file):
+				shutil.move(self.graph_login,dst)
+			else:
+				logging.warning("Login png already exists.To be convered.")
+				os.remove(dst_file)
+				shutil.move(self.graph_login,dst)
